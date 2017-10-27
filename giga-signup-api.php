6d2a96c8562e20803cc3d05b0e06230c6204f626
Rich Lott <forums@artfulrobot.uk>
2017-10-27 08:01:43 +0100

    Add documentation

--- giga-signup-api.php
+++ giga-signup-api.php
@@ -4,12 +4,161 @@
  *
  * @author Rich Lott / Artful Robot for Systopia.
  *
- * Note:
- * Pre-shared key must be defined before the API is called, e.g. in
+ * Configuration
+ * =============
+ *
+ * The pre-shared key must be defined before the API is called, e.g. in
  * your settings config file. Important that the file it is defined in is
  * never checked into a public repository like github!
  *
  * eg. define('GIGA_EMAIL_SUBSCRIPTION_API_PSK', 'insert-some-hash-here');
+ *
+ * Usage
+ * =====
+ *
+ * require_once() this file. Then you can make calls like:
+ *
+ *     $result = GigaEmailSubscriptionAPI::request('getContactData', $params);
+ *     $result = GigaEmailSubscriptionAPI::request('setContactData', $params);
+ *     $result = GigaEmailSubscriptionAPI::request('addSubscriber', $params);
+ *     $result = GigaEmailSubscriptionAPI::request('getContactHash', $params);
+ *
+ * You should check !empty($result['error']) to see if there was a problem.
+ *
+ * Most functions require a contact hash to authenticate and email. We will
+ * have to ensure this is created by CiviCRM's mailing and present in the URL.
+ * e.g.
+ *
+ * https://your-web-server.com/subscription-updates?email=foo%46example.com&hash=aabb112233dd4411dbsfkjdfhsjdh
+ *
+ * getContactData
+ * --------------
+ *
+ * $params = ['hash' => $_GET['hash'], 'email' => $_GET['email']];
+ * $result = GigaEmailSubscriptionAPI::request('getContactData', $params);
+ *
+ * Result: Array(
+ *     [contact_id] => 12345
+ *     [first_name] => Wilma
+ *     [last_name] => Flintstone
+ *     [prefix_id] =>
+ *     [individual_prefix] => Ms.
+ *     [id] => 12345
+ *     [email] => wilma@example.com
+ *     [giga_en_latinamerica] => 0
+ *     [giga_de_latinamerica] => 0
+ *     [giga_en_middleeast] => 0
+ *     [giga_de_middleeast] => 0
+ *     [giga_en_asia] => 0
+ *     [giga_de_asia] => 0
+ *     [giga_en_global] => 0
+ *     [giga_de_global] => 0
+ *     [giga_en_africa] => 0
+ *     [giga_de_afrika] => 0
+ *     [journal_africa_spectrum_de] => 0
+ *     [journal_africa_spectrum_en] => 0
+ *     [journal_chinese_affairs_de] => 0
+ *     [journal_chinese_affairs_en] => 0
+ *     [journal_latin_america_de] => 0
+ *     [journal_latin_america_en] => 0
+ *     [journal_se_asia_de] => 0
+ *     [journal_se_asia_en] => 0
+ *     [working_papers_en] => 0
+ *     [working_papers_de] => 0
+ * )
+ *
+ * Possible Errors
+ *
+ * - Unauthorised. Invalid hash.
+ * - Bad Request. email missing
+ * - Bad Request. Problem with input email
+ *   email not found, or belongs to 2+ contacts.
+ *
+ * setContactData
+ * --------------
+ *
+ * $params = [
+ *  // As with getContactData():
+ *  'hash' => $_GET['hash'],
+ *  'email' => $_GET['email'],
+ *  // All that follow are optional. If you don't provide one, no change will happen.
+ *  // Change subscriptions like this:
+ *  'giga_en_latinamerica' => 1, // Subscribe to this list
+ *  'giga_de_latinamerica' => 0, // Unsubscribe from this list
+ *  // You can also change contact details.
+ *  'first_name'        => 'Fred',
+ *  'last_name'         => 'Flintstone',
+ *  'individual_prefix' => 'Mr.', // must be registered with CiviCRM
+ *  'new_email'         => 'new@example.com', // Note 'new_email'
+ * ];
+ * $result = GigaEmailSubscriptionAPI::request('getContactData', $params);
+ *
+ * Normally the result is an empty array []. However you should check for
+ * $result['errors'] which could be one of the following:
+ *
+ * - Unauthorised. Invalid hash.
+ * - Bad Request. email missing
+ * - Bad Request. Problem with input email
+ *   email not found, or belongs to 2+ contacts.
+ * - Bad Request. Unknown prefix
+ *   Each individual_prefix must be registered in CiviCRM.
+ *
+ * getContactHash
+ * --------------
+ *
+ * This enables you to generate authentication link for the given email.
+ *
+ * $params = ['email' => $_GET['email']];
+ * $result = GigaEmailSubscriptionAPI::request('getContactHash', $params);
+ *
+ * // Over-simplified example:
+ * mail($_GET['email'],
+ *   'Update your mailing preferences',
+ *   'You need this link: https://your-web-server.com/subscription-updates'
+ *   . 'email=' . urlencode($_GET['email']) . '&hash=' . $result['hash']
+ *   );
+ *
+ * Possible Errors
+ *
+ * - Bad Request. email missing
+ * - Bad Request. Problem with input email
+ *   email not found, or belongs to 2+ contacts.
+ *
+ * addSubscriber
+ * -------------
+ *
+ * If you are adding a new subscriber you won't have a hash or existing email
+ * and you must use this method. $params is the same as for setContactData()
+ * except that you don't supply the 'hash' and 'email' keys. The email must be
+ * provided as 'new_email'.
+ *
+ * $result = GigaEmailSubscriptionAPI::request('addSubscriber', $params);
+ *
+ * As with setContactData(), the result is normally an empty array []. However
+ * you should check for $result['errors'] which could be one of the following:
+ *
+ * - Unauthorised. Invalid hash.
+ * - Bad Request. email missing
+ * - Bad Request. Problem with input email
+ *   email not found, or belongs to 2+ contacts.
+ * - Bad Request. Unknown prefix
+ *   Each individual_prefix must be registered in CiviCRM.
+ * - Bad Request. Email is required to create a contact.
+ * - Bad Request. At least one subscription is required.
+ * - Bad Request. Name is required to create a contact.
+ * - Authentication Required
+ *
+ * The last one of those errors is important. It means that you tried to send
+ * data for a contact that is already in the database. We cannot allow updating
+ * data of existing contacts through the addSubscriber API because otherwise it
+ * would be possible for a malicious user to change someone else's data. This
+ * is the purpose of the hash in setContactData(). So if you try to add a contact
+ * that already exists you'll get this error.
+ *
+ * However you will also be sent the hash for that contact, in $result['hash']
+ * so you can send them an email with the authentication link in which should
+ * allow them to subscribe.
+ *
  */
 if (!defined('GIGA_EMAIL_SUBSCRIPTION_API_ENDPOINT')) {
   define('GIGA_EMAIL_SUBSCRIPTION_API_ENDPOINT', 'https://crm.giga-hamburg.de/civicrm/giga-subscriptions-api');
@@ -19,7 +168,7 @@ if (!defined('GIGA_EMAIL_SUBSCRIPTION_API_ENDPOINT')) {
  * will need to be checked for in $output['error']
  */
 if (!defined('GIGA_EMAIL_SUBSCRIPTION_API_NO_THROW')) {
-  define('GIGA_EMAIL_SUBSCRIPTION_API_NO_THROW', FALSE);
+  define('GIGA_EMAIL_SUBSCRIPTION_API_NO_THROW', TRUE);
 }
 
 /**
--- test.php
+++ test.php
@@ -1,4 +1,5 @@
 <?php
+exit(); // This is a hacky script used by developers in testing. It is not portable.
 /**
  * Examples.
  */
@@ -9,20 +10,23 @@ define('GIGA_EMAIL_SUBSCRIPTION_API_PSK', 'aabbccddeeff');
 
 include './giga-signup-api.php';
 
+// The test contact's email.
+$email = 'foo@example.com';
 
-//$x = GigaEmailSubscriptionAPI::request('getContactData', ['email' => 'forums@artfulrobot.uk', 'hash' => $result['hash']]);
-if (FALSE) {
-$result = GigaEmailSubscriptionAPI::request('getContactHash', ['email' => 'forums@artfulrobot.uk']);
+// Various example calls here. Comment out appropriately.
+
+$result = GigaEmailSubscriptionAPI::request('getContactHash', ['email' => $email]);
+$x = GigaEmailSubscriptionAPI::request('getContactData', ['email' => $email, 'hash' => $result['hash']]);
+$result = GigaEmailSubscriptionAPI::request('getContactHash', ['email' => $email]);
 // This should return an error.
 $x = GigaEmailSubscriptionAPI::request('getContactData', ['email' => 'forumsc@artfulrobot.uk', 'hash' => $result['hash']]);
 // This should update the contact.
-$x = GigaEmailSubscriptionAPI::request('setContactData', ['email' => 'forums@artfulrobot.uk', 'hash' => $result['hash'], 'first_name' => 'Rich']);
+$x = GigaEmailSubscriptionAPI::request('setContactData', ['email' => $email, 'hash' => $result['hash'], 'first_name' => 'Rich']);
 // This should not return an error.
-$x = GigaEmailSubscriptionAPI::request('getContactData', ['email' => 'forums@artfulrobot.uk', 'hash' => $result['hash']]);
-$x = GigaEmailSubscriptionAPI::request('addSubscriber', ['new_email' => 'forums@artfulrobot.uk']);
-$x = GigaEmailSubscriptionAPI::request('addSubscriber', ['new_email' => 'forums2@artfulrobot.uk', 'giga_en_latinamerica' => 1, 'first_name' => 'Reech', 'last_name' => 'loot']);
-}
-$result = GigaEmailSubscriptionAPI::request('getContactHash', ['email' => 'forums@artfulrobot.uk']);
+$x = GigaEmailSubscriptionAPI::request('getContactData', ['email' => $email, 'hash' => $result['hash']]);
+$x = GigaEmailSubscriptionAPI::request('addSubscriber', ['new_email' => $email]);
+$x = GigaEmailSubscriptionAPI::request('addSubscriber', ['new_email' => 'doesnotexist@example.com', 'giga_en_latinamerica' => 1, 'first_name' => 'Reech', 'last_name' => 'loot']);
+$result = GigaEmailSubscriptionAPI::request('getContactHash', ['email' => $email]);
 // This should update the contact.
-$x = GigaEmailSubscriptionAPI::request('setContactData', ['email' => 'forums@artfulrobot.uk', 'hash' => $result['hash'], 'individual_prefix' => 'Monkey']);
+$x = GigaEmailSubscriptionAPI::request('setContactData', ['email' => $email, 'hash' => $result['hash'], 'individual_prefix' => 'Monkey']);
 print_r($x);
